139582ec7183ce957026e4369143b7a1907b6029
Dakota Kallas <kallas6990@uwlax.edu>
2023-04-09 17:06:36 -0500

    Improved code reuse

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,51 +10,51 @@ let Game = require("../models/game.js");
 const saltRounds = 10;
 const BGAClientID = "byh1ZsZ8eh";
 
-// UserDB.createUser(
-//   "dakota@test.com",
-//   bcrypt.hashSync("123", saltRounds),
-//   "Dakota",
-//   "Kallas",
-//   true,
-//   true
-// )
-//   .then((user) => {
-//     console.log(`Created User document for email ${user.email}`);
-//   })
-//   .catch((error) => {
-//     console.error("Error creating GameShelf document:", error);
-//   });
+UserDB.createUser(
+  "dakota@test.com",
+  bcrypt.hashSync("123", saltRounds),
+  "Dakota",
+  "Kallas",
+  true,
+  true
+)
+  .then((user) => {
+    console.log(`Created User document for email ${user.email}`);
+  })
+  .catch((error) => {
+    console.error("Error creating GameShelf document:", error);
+  });
 
-// GameShelfDB.createGameShelf("dakota@test.com", [])
-//   .then((gameshelf) => {
-//     console.log(`Created GameShelf document for owner ${gameshelf.owner}`);
-//   })
-//   .catch((error) => {
-//     console.error("Error creating GameShelf document:", error);
-//   });
+GameShelfDB.createGameShelf("dakota@test.com", [])
+  .then((gameshelf) => {
+    console.log(`Created GameShelf document for owner ${gameshelf.owner}`);
+  })
+  .catch((error) => {
+    console.error("Error creating GameShelf document:", error);
+  });
 
-// UserDB.createUser(
-//   "other@test.com",
-//   bcrypt.hashSync("123", saltRounds),
-//   "Test",
-//   "User",
-//   true,
-//   false
-// )
-//   .then((user) => {
-//     console.log(`Created User document for email ${user.email}`);
-//   })
-//   .catch((error) => {
-//     console.error("Error creating GameShelf document:", error);
-//   });
+UserDB.createUser(
+  "other@test.com",
+  bcrypt.hashSync("123", saltRounds),
+  "Test",
+  "User",
+  true,
+  false
+)
+  .then((user) => {
+    console.log(`Created User document for email ${user.email}`);
+  })
+  .catch((error) => {
+    console.error("Error creating GameShelf document:", error);
+  });
 
-// GameShelfDB.createGameShelf("other@test.com", [])
-//   .then((gameshelf) => {
-//     console.log(`Created GameShelf document for owner ${gameshelf.owner}`);
-//   })
-//   .catch((error) => {
-//     console.error("Error creating GameShelf document:", error);
-//   });
+GameShelfDB.createGameShelf("other@test.com", [])
+  .then((gameshelf) => {
+    console.log(`Created GameShelf document for owner ${gameshelf.owner}`);
+  })
+  .catch((error) => {
+    console.error("Error creating GameShelf document:", error);
+  });
 
 // Routes
 
@@ -77,54 +77,25 @@ router.get("/gameshelf", async function (req, res) {
   try {
     let gameshelf = await GameShelfDB.getByOwner(req.session.user.email);
     let idString = "";
+
     gameshelf.games.forEach(
       (bgaGameId) => (idString = `${idString},${bgaGameId}`)
     );
+
     if (idString != "") {
-      fetch(
-        `https://api.boardgameatlas.com/api/search?client_id=${BGAClientID}&ids=${idString}`
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.games && data.games.length > 0) {
-            let games = [];
-
-            data.games.forEach((game) => {
-              const currentGame = new Game(
-                game.id,
-                game.name,
-                Math.round(game.average_user_rating * 2) / 2,
-                game.images.small,
-                game.image_url,
-                game.min_players,
-                game.max_players,
-                game.year_published,
-                game.playtime ?? "--",
-                game.plays,
-                game.rank,
-                game.trending_rank,
-                game.description,
-                game.min_age,
-                game.rules_url,
-                game.primary_publisher?.name
-              );
-              games.push(currentGame);
-            });
-
-            gameshelf.games = games;
-            res.status(200).send(gameshelf);
-          } else {
-            res.status(404).send("Game(s) were not found, try again later.");
-          }
-        })
-        .catch((err) => {
-          res.status(400).send(err.message);
-        });
-    } else {
-      res.status(200).send(gameshelf);
+      let games = await getGames(idString);
+      if (games) {
+        gameshelf.games = games;
+      } else {
+        throw Error(
+          "There was an issue collecting the Game Shelf, try again later."
+        );
+      }
     }
+
+    res.status(200).send(gameshelf);
   } catch (err) {
-    res.status(400).send(err.message);
+    res.status(404).send(err.message);
   }
 });
 
@@ -136,6 +107,7 @@ router.put("/gameshelf", async function (req, res) {
     const game = req.body.game;
     if (game) {
       let gameShelf = await GameShelfDB.getByOwner(req.session.user.email);
+
       if (
         gameShelf &&
         gameShelf.games.find((result) => result === game.bgaGameId)
@@ -154,48 +126,17 @@ router.put("/gameshelf", async function (req, res) {
             (bgaGameId) => (idString = `${idString},${bgaGameId}`)
           );
         }
+
         if (idString != "") {
-          fetch(
-            `https://api.boardgameatlas.com/api/search?client_id=${BGAClientID}&ids=${idString}`
-          )
-            .then((response) => response.json())
-            .then((data) => {
-              if (data.games && data.games.length > 0) {
-                let games = [];
-
-                data.games.forEach((game) => {
-                  const currentGame = new Game(
-                    game.id,
-                    game.name,
-                    Math.round(game.average_user_rating * 2) / 2,
-                    game.images.small,
-                    game.image_url,
-                    game.min_players,
-                    game.max_players,
-                    game.year_published,
-                    game.playtime ?? "--",
-                    game.plays,
-                    game.rank,
-                    game.trending_rank,
-                    game.description,
-                    game.min_age,
-                    game.rules_url,
-                    game.primary_publisher?.name
-                  );
-                  games.push(currentGame);
-                });
-
-                updatedShelf.games = games;
-                res.status(200).send(updatedShelf);
-              } else {
-                res
-                  .status(404)
-                  .send("Game(s) were not found, try again later.");
-              }
-            })
-            .catch((err) => {
-              res.status(400).send(err.message);
-            });
+          let games = await getGames(idString);
+          if (games) {
+            updatedShelf.games = games;
+            res.status(200).send(updatedShelf);
+          } else {
+            throw Error(
+              "There was an issue providing the game, try again later."
+            );
+          }
         } else {
           res.status(200).send(updatedShelf);
         }
@@ -219,44 +160,18 @@ router.delete("/gameshelf/:gid", async function (req, res) {
         req.session.user.email,
         bgaGameId
       );
-      fetch(
-        `https://api.boardgameatlas.com/api/search?client_id=${BGAClientID}&ids=${removedGameBGAId}`
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.games && data.games.length > 0) {
-            const game = data.games[0];
-            const removedGame = new Game(
-              game.id,
-              game.name,
-              Math.round(game.average_user_rating * 2) / 2,
-              game.images.small,
-              game.image_url,
-              game.min_players,
-              game.max_players,
-              game.year_published,
-              game.playtime ?? "--",
-              game.plays,
-              game.rank,
-              game.trending_rank,
-              game.description,
-              game.min_age,
-              game.rules_url,
-              game.primary_publisher?.name
-            );
-            res.status(200).send(removedGame);
-          } else {
-            res.status(404).send("The game was not found, try again later.");
-          }
-        })
-        .catch((err) => {
-          res.status(400).send(err.message);
-        });
+
+      let games = await getGames(removedGameBGAId);
+      if (games) {
+        res.status(200).send(games[0]);
+      } else {
+        throw Error("There was an issue removing the game, try again later.");
+      }
     } else {
       throw Error("There was an issue providing the game, try again later.");
     }
   } catch (err) {
-    res.status(400).send(err.message);
+    res.status(404).send(err.message);
   }
 });
 
@@ -316,41 +231,62 @@ router.get("/search", async function (req, res) {
   }
 });
 
+/**
+ * GET GAME DETAILS
+ */
 router.get("/games/:gid", async function (req, res) {
   const bgaGameId = req.params.gid;
-  fetch(
-    `https://api.boardgameatlas.com/api/search?client_id=${BGAClientID}&ids=${bgaGameId}`
+  let games = await getGames(bgaGameId);
+  if (games) {
+    res.status(200).send(games[0]);
+  } else {
+    res.status(400).send("Unable to retrieve game details, try agai later.");
+  }
+});
+
+/**
+ * Helper method used to fetch game(s) from the BoardGameAtlas API
+ * @param {String[]} idListString
+ * @returns {Game[]}
+ */
+async function getGames(idListString) {
+  return fetch(
+    `https://api.boardgameatlas.com/api/search?client_id=${BGAClientID}&ids=${idListString}`
   )
     .then((response) => response.json())
     .then((data) => {
       if (data.games && data.games.length > 0) {
-        const game = data.games[0];
-        const currentGame = new Game(
-          game.id,
-          game.name,
-          Math.round(game.average_user_rating * 2) / 2,
-          game.images.small,
-          game.image_url,
-          game.min_players,
-          game.max_players,
-          game.year_published,
-          game.playtime ?? "--",
-          game.plays,
-          game.rank,
-          game.trending_rank,
-          game.description,
-          game.min_age,
-          game.rules_url,
-          game.primary_publisher?.name
-        );
-        res.status(200).send(currentGame);
+        let games = [];
+
+        data.games.forEach((game) => {
+          const currentGame = new Game(
+            game.id,
+            game.name,
+            Math.round(game.average_user_rating * 2) / 2,
+            game.images.small,
+            game.image_url,
+            game.min_players,
+            game.max_players,
+            game.year_published,
+            game.playtime ?? "--",
+            game.plays,
+            game.rank,
+            game.trending_rank,
+            game.description,
+            game.min_age,
+            game.rules_url,
+            game.primary_publisher?.name
+          );
+          games.push(currentGame);
+        });
+        return games;
       } else {
-        res.status(404).send("The game was not found, try again later.");
+        return null;
       }
     })
     .catch((err) => {
-      res.status(400).send(err.message);
+      return null;
     });
-});
+}
 
 module.exports = router;
